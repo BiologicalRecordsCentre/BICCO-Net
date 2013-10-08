34a8808a3c2a2c301654f60e758929dae9b7f896
blaisemartay <blaise.martay@bto.org>
2013-10-08 14:51:22 +0100

    extra annotation2

--- scripts/test_example.R
+++ scripts/test_example.R
@@ -31,10 +31,62 @@ fake_fourier <- read.csv(paste0(datadir,"/fourier_example.csv"),header=T)		# add
 sp8 <- merge(sp8,fake_fourier,by.x=c("SITE","YEAR",by.y=c("SITE","YEAR")))			# for species 8
 sp58 <- merge(sp58,fake_fourier,by.x=c("SITE","YEAR",by.y=c("SITE","YEAR")))		# for species 58
 
-############# missing step
-#### caulculate LOCAL Temp and Precipitation effect from:
-# 1. species-specific fourier covariates 
-# 2. local P & T
+
+###########################################################################
+
+###### converting local climate into difference from national averages ######
+
+climate<-fakeweather
+climate1<-fakeweather[,c(13:2,25:14)]
+
+
+# setting up site-specific climate data in required format:
+# each row contains temperature and precipitation from year of survey to five years before the survey
+climate2<-cbind(Year[7:52],
+                climate1[6:51,1:12],climate1[5:50,1:12],climate1[4:49,1:12],
+                climate1[3:48,1:12],climate1[2:47,1:12],climate1[1:46,1:12],
+                climate1[6:51,13:24],climate1[5:50,13:24],climate1[4:49,13:24],
+                climate1[3:48,13:24],climate1[2:47,13:24],climate1[1:46,13:24])                
+climate3<-matrix(0,46,144)
+
+
+# transforming site-specific climate to difference from UK mean (scaled  by variance)
+for(i in 1:46){ for(j in 1:144){
+  climate3[i,j]<-(climate2[i,j+1]-transdata[1,j+1])/transdata[2,j+1]}}
+
+
+### getting species response to each month's weather then averaging for annual effect:
+weatherCovs<-matrix(0,46,144)
+for(i in 1:46){ for(j in 1:144){
+  weatherCovs[i,j]<-as.matrix(climate3[i,j]*spCovariates[sp,j+1])}}
+year<-c(1966:2011)
+
+temp<-rowMeans(weatherCovs[,(13-spCovariates[sp,146]):(72-spCovariates[sp,146])])
+precipitation<-rowMeans(weatherCovs[,((13-spCovariates[sp,146]):(72-spCovariates[sp,146])+72)])
+
+
+# annualweather variables (temp & precipitation) are an annual estimation of 
+# population growth in species[sp] due to temperature and rainfall.
+annualweather<-as.data.frame(cbind(c(1966:2011),temp,precipitation))
+names(annualweather)<-c("year","temp","precipitation")
+attach(annualweather)
+
+
+# The interaction between these variables and habitat variable(s) should show
+# whether habitat affects species' response to weather.
+
+###########################################################################
+### Missing step NEW:
+
+### 'temp' and 'precipitation' give the annual effect for species and site 
+### selected in lines 44:47 from 1966 - 2011. 
+### A loop will need to be added to produce these values for each site
+
+### select years required for glmers:
+### will need to be change to do it for each site
+TEMP1<-temp[match(sp8$YEAR[sp8$SITE==site],year)]
+PREC1<-precipitation[match(sp8$YEAR[sp8$SITE==site],year)]
+
 ############# 
 
 ###  Mixed effects models 
